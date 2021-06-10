83d74c6b690684f2e58b007f0c0dd2b77b3d5893
jayashree-1998 <54501598+jayashree-1998@users.noreply.github.com>
2021-06-10 23:15:33 +0530

    Update CropFactory.sol

--- contracts/CropFactory.sol
+++ contracts/CropFactory.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
-import "./Crop.sol";
 
+import "./Crop.sol";
+import "./GovtInput.sol";
 contract CropFactory{
-	uint i = 0;
+	uint j = 0;
 	struct ContractAddresses{
 		address crop;
 		address govrep;
 	}
 	ContractAddresses[] public contractaddresses;
-	address[] public farmerAddresses;
 
     mapping (address => ContractAddresses) public farmerContractMapping;
+    mapping (uint => address) public farmerAddresses;
 
     function getContractsForFarmer() public view returns(ContractAddresses memory){
         return(farmerContractMapping[msg.sender]);
@@ -23,17 +24,12 @@ contract CropFactory{
 		newCrop.setAddress(address(newGovrep));
 		newGovrep.setAddress(address(newCrop));
         farmerContractMapping[msg.sender] = ContractAddresses(address(newCrop),address(newGovrep));
-		farmerAddresses[i] = msg.sender; 
-		i++;
+        farmerAddresses[j] = msg.sender; 
+        contractaddresses.push(farmerContractMapping[farmerAddresses[j]]);
+		j++;
     }
     
 	function getFarmerContractMapping() public view returns(ContractAddresses[] memory){
-	   /* ContractAddresses[] memory contrract = new ContractAddresses[](farmerAddresses.length);
-	    for(uint i = 0; i < farmerAddresses.length; i++)
-	    {
-	        ContractAddresses storage contrract = contractaddresses[i];
-	        contrract[i] = contrract;
-	    } */
 	    return contractaddresses;
 	}
 }
